dc71353619645517f3887768130ee13064fc7a6e
tomosaigon <tomo@crowdeo.com>
2021-10-09 19:42:59 +0700

    new component

---
+++ packages/react-app/src/AvailableCollateral.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+const { utils } = require("ethers");
+import "./BoredPunks.css"
+
+
+export default function AvailableCollateral(props) {
+
+
+  return (
+    <div className='availableCollateral'>
+      <div className='collateralCard'>
+      </div>
+    </div>
+
+  );
+
+}
