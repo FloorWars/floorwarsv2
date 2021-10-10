b32d26346f4e8d4cf98631df4a5b7696c5b41c8b
tomosaigon <tomo@crowdeo.com>
2021-10-11 00:52:03 +0700

    Add simulator for expiry value of long token

--- packages/react-app/src/views/BoredPunks.jsx
+++ packages/react-app/src/views/BoredPunks.jsx
@@ -1,6 +1,6 @@
 import { SyncOutlined } from "@ant-design/icons";
 import { utils } from "ethers";
-import { Button, Card, Collapse, DatePicker, Divider, Input, List, Progress, Slider, Spin, Steps, Switch, Typography, Layout, Menu, Breadcrumb } from "antd";
+import { Button, Card, Col, Collapse, DatePicker, Divider, Input, InputNumber, List, Progress, Row, Slider, Spin, Steps, Switch, Typography, Layout, Menu, Breadcrumb } from "antd";
 import React, { useState } from "react";
 import { Address, Balance, BoredPunksApp, SwapInfo } from "../components";
 import { useContractReader } from "eth-hooks"
@@ -22,7 +22,6 @@ export default function BoredPunks({
   readContracts,
   writeContracts,
 }) {
-  const [amount, setNewAmount] = useState(0);
   // const [longAmount, setLongAmount] = useState(0);
   // const [shortAmount, setShortAmount] = useState(0);
 
@@ -30,6 +29,7 @@ export default function BoredPunks({
   // const formattedShort = shortBalance / (10**6)
   // const formattedBalance = usdcBalance / (10**6)
 
+  const [slideAmount, setSlideAmount] = useState(400000);
 
   return (
 
@@ -44,6 +44,28 @@ export default function BoredPunks({
            <Paragraph>
              <code>USDfw10PUNKc800-1221</code> is a covered call option on USD versus the Decile Floor CryptoPunks index price converted from ETH to USD. Token pairs are minted and collateralized by 1 USDC and represent 1 USDC worth of risk versus the price of USD in fw10PUNK at expiry. Long tokens expire worthless if more than $800,000 USDC is needed for 1 theoretical "fw10PUNK" otherwise are valued at the difference proportionate to the expiry price. For example, if fw10PUNKUSD (the inverse price) settles at $400,000 on December 31st, each long token will be worth: <code>(1/200000 - 1/800000)/(1/200000) = 0.75</code> so $0.75 USDC per token. Short tokens are worth the remainder of the $1 of collateral, e.g. <code>1 - 0.75 = 0.25</code> so $0.25. 
            </Paragraph>
+           <Paragraph>Let's simulate how the long token settles as the USD price of 1 fw10PUNK changes.
+           </Paragraph>
+           <Row>
+             <Col span={12}>
+               <Slider
+                 min={1}
+                 max={800000}
+                 onChange={setSlideAmount}
+                 value={typeof slideAmount === 'number' ? slideAmount : 1}
+               />
+             </Col>
+             <Col span={4}>
+               <InputNumber
+                 min={1}
+                 max={800000}
+                 style={{ margin: '0 16px' }}
+                 value={slideAmount}
+                 onChange={setSlideAmount}
+               />
+             </Col>
+            <Paragraph>1 Long token is worth: { (1/slideAmount - 1/800000)/(1/slideAmount) } of $1 collateral</Paragraph>
+           </Row>
          </Typography>
          <Collapse defaultActiveKey={['1']} >
            <Panel header="Steps to create a synthetic position on CryptoPunks" key="1">
