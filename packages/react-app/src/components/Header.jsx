cdc27c7b224501ac596ca2bf843b82659e4c29bf
tomosaigon <tomo@crowdeo.com>
2021-10-10 23:21:22 +0700

    Logo

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import { PageHeader } from "antd";
 import React from "react";
 
+
 // displays a page header
 
 export default function Header() {
   return (
     <a href="https://floorwars.github.io" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="FloorWars"
+        title={(<img src='/logo.png' width='200' />)} 
         subTitle="Synthetic NFT Floor Derivatives"
         style={{ cursor: "pointer" }}
       />
