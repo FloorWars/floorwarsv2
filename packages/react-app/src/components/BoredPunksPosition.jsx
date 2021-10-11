c622fae85bd10188175f75eb1ca50fd9c5353012
ask03 <aykim043@gmail.com>
2021-10-11 19:58:18 +0900

    mint show span

--- packages/react-app/src/components/BoredPunksPosition.jsx
+++ packages/react-app/src/components/BoredPunksPosition.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 const { utils } = require("ethers");
-import { Typography, Row, Col, Button, Space } from "antd";
+import { Typography, Row, Col, Button, Space, Input } from "antd";
 import "./BoredPunks.css";
 
 const { Title, Paragraph, Text, Link } = Typography;
 
 export default function BoredPunksPosition(props) {
+  const [createAmount, setCreateAmount] = useState(0);
+  const [showSpan, setShowSpan] = useState(false);
 
   let address = props.address;
-  let positionTokens = [];
   let colBalance = props.usdcBalance ? utils.formatUnits(props.usdcBalance, 6) : 0;
   let longBalance = props.longBalance ? utils.formatUnits(props.longBalance, 6) : 0;
   let shortBalance = props.shortBalance ? utils.formatUnits(props.shortBalance, 6) : 0;
   let pairsMinted = props.pairsMinted ? utils.formatUnits(props.pairsMinted, 6) : 0;
   let colAllowance = props.colAllowance ? utils.formatUnits(props.colAllowance, 6) : 0;
+
 
   return (
     <div className="BoredPunksPosition">
@@ -29,12 +31,23 @@ export default function BoredPunksPosition(props) {
             <Text strong>Short Tokens:</Text>
             <br></br>
             <Text strong>Pairs Minted:</Text>
+            <br></br>
+            { showSpan ?  <span>Amount exceeds allowance</span> : <br></br> }
+            <Input placeholder="L/S amount" onChange={e => {
+              setCreateAmount(e.target.value)
+
+              if(createAmount > colAllowance) {
+                setShowSpan(true)
+              } else {
+                setShowSpan(false)
+              }
+              console.log("showspan", showSpan)
+            }} />
             <Button type="primary"
               onClick={async () => {
-                const result = props.tx(props.writeContracts.LSP.create(utils.parseUnits(colAllowance, 6)))
+                const result = props.tx(props.writeContracts.LSP.create(utils.parseUnits(createAmount, 6)))
               }}
                 block>Mint L/S Tokens</Button>
-            <Button block>Settle L/S Tokens</Button>
           </Col>
           <Col span ={12}>
             <br></br>
@@ -46,6 +59,9 @@ export default function BoredPunksPosition(props) {
             <Text strong>{shortBalance}</Text>
             <br></br>
             <Text strong>{pairsMinted}</Text>
+            <br></br>
+            <br></br>
+
             <Button type="primary"
               onClick={async () => {
                 const result = props.tx(props.writeContracts.LSP.redeem(10000000))
@@ -53,6 +69,7 @@ export default function BoredPunksPosition(props) {
             danger block>
               Redeem
             </Button>
+              <Button type="primary" block>Settle L/S Tokens</Button>
           </Col>
         </Row>
     </div>
