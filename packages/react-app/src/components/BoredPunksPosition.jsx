6384aeae947c5090aa843dcd02b85f3b30e63e83
tomosaigon <tomo@crowdeo.com>
2021-10-12 18:51:30 +0700

    Restyle position section

--- packages/react-app/src/components/BoredPunksPosition.jsx
+++ packages/react-app/src/components/BoredPunksPosition.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 const { utils } = require("ethers");
-import { Typography, Row, Col, Button, Space, Input, Spin } from "antd";
+import { Card, Descriptions, Divider, List, Typography, Row, Col, Button, Space, Input, Spin } from "antd";
+
 import "./BoredPunks.css";
 
 const { Title, Paragraph, Text, Link } = Typography;
@@ -48,86 +49,83 @@ export default function BoredPunksPosition(props) {
   return (
     <div className="BoredPunksPosition">
         <Row justify="center">
-          <Col span = {12}>
-            <Text strong>Your Wallet Balance</Text>
-            <br></br>
-            <br></br>
-            <Text strong>Collateral(USDC):</Text>
-            <br></br>
-            <Text strong>Long Tokens:</Text>
-            <br></br>
-            <Text strong>Short Tokens:</Text>
-            <br></br>
-            <br></br>
-            <Text>Available allowance: {colAllowance}</Text>
-            <Input value={createAmount} type="float" onChange={e => {
-              setCreateAmount(e.target.value)
+          <Divider>Create or Redeem Collateralized Long & Short Pairs</Divider>
+          <Col span = {11}>
+            <Card >
+              <Descriptions title="Open Long & Short Position" bordered>
+                <Descriptions.Item label="Collateral" span={3}>{colBalance}</Descriptions.Item>
+                <Descriptions.Item label="Approved" span={3}>{colAllowance}</Descriptions.Item>
+              </Descriptions>
+              <Input value={createAmount} type="float" onChange={e => {
+                setCreateAmount(e.target.value)
 
-              if(parseFloat(e.target.value) > parseFloat(colAllowance)) {
-                setShowSpan(true)
-              } else {
-                setShowSpan(false)
-              }
-              if(e.target.value === '') {
-                setShowSpan(false)
-              }
+                if(parseFloat(e.target.value) > parseFloat(colAllowance)) {
+                  setShowSpan(true)
+                } else {
+                  setShowSpan(false)
+                }
+                if(e.target.value === '') {
+                  setShowSpan(false)
+                }
 
-            }} />
-            <Button type="primary"
-              onClick={async () => {
-                if(!showSpan) {
-                  if(createAmount <= 0) {
-                    window.alert("Enter a value to get approval or create")
+               }} />
+              <Button type="primary"
+                onClick={async () => {
+                  if(!showSpan) {
+                    if(createAmount <= 0) {
+                      window.alert("Enter a value to get approval or create")
+                    } else {
+                      setShowSpin(true)
+
+                      const result = await props.tx(props.writeContracts.LSP.create(utils.parseUnits(createAmount, 6)))
+                      .then((e) => setShowSpin(false))
+                    }
+
                   } else {
+
+                    let approvalAmount = createAmount
+                    approvalAmount = utils.parseUnits(approvalAmount.toString(), 6)
                     setShowSpin(true)
 
-                    const result = await props.tx(props.writeContracts.LSP.create(utils.parseUnits(createAmount, 6)))
+                    const lspAddress = props.readContracts && props.readContracts.LSP && props.readContracts.LSP.address;
+                    const resultApproval = await props.tx(props.writeContracts.USDC.approve(lspAddress, approvalAmount))
                     .then((e) => setShowSpin(false))
+
                   }
 
-                } else {
-
-                  let approvalAmount = createAmount
-                  approvalAmount = utils.parseUnits(approvalAmount.toString(), 6)
-                  setShowSpin(true)
-
-                  const lspAddress = props.readContracts && props.readContracts.LSP && props.readContracts.LSP.address;
-                  const resultApproval = await props.tx(props.writeContracts.USDC.approve(lspAddress, approvalAmount))
-                  .then((e) => setShowSpin(false))
-
-                }
-
-              }}
-                block>{showSpan ? (showSpin ? <Spin /> : "Approve") : (showSpin ? <Spin /> : "Create")}</Button>
+                }}
+                  block>{showSpan ? (showSpin ? <Spin /> : "Approve") : (showSpin ? <Spin /> : "Create")}</Button>
+            </Card>
           </Col>
-          <Col span ={12}>
-            <br></br>
-            <br></br>
-            <Text strong>{colBalance}</Text>
-            <br></br>
-            <Text strong>{longBalance}</Text>
-            <br></br>
-            <Text strong>{shortBalance}</Text>
-            <br></br>
-            <br></br>
-            <Text>Max redeem amount: {maxRedeem}</Text>
-            <Input type="float" value={redeemVal} onChange={e => {
-              setRedeemVal(e.target.value)
-            }}></Input>
-            <Button type="primary"
-              onClick={async () => {
-                if(redeemVal > maxRedeem || redeemVal <= 0) {
-                  window.alert("Select a valid redeem amount")
-                } else {
-                  setShowSpin(true)
-                  const result = await props.tx(props.writeContracts.LSP.redeem(utils.parseUnits(redeemVal.toString(), 6)))
-                  .then((e) => setShowSpin(false))
-                }
-              }}
-            danger>
-              { showSpin ? <Spin /> : "Redeem" }
-            </Button>
-              <Button type="primary">  { showSpin ? <Spin /> : "Settle" }</Button>
+          <Col span={1}></Col>
+          <Col span = {12}>
+            <Card >
+              <Descriptions title="Close Long or Short Position" bordered>
+                <Descriptions.Item label="Long" span={3}>{longBalance}</Descriptions.Item>
+                <Descriptions.Item label="Short" span={3}>{shortBalance}</Descriptions.Item>
+                <Descriptions.Item label="Redeem Pairs" span={3}>{maxRedeem}</Descriptions.Item>
+                <Descriptions.Item label="Expired" span={3}>No</Descriptions.Item>
+              </Descriptions>
+              {/*<Input type="number" placeholder='Long tokens to spend'/>
+              <Input type="number" placeholder='Short tokens to spend'/>*/}
+              <Input type="float" value={redeemVal} onChange={e => {
+                setRedeemVal(e.target.value)
+              }}></Input>
+              <Button type="primary"
+                onClick={async () => {
+                  if(redeemVal > maxRedeem || redeemVal <= 0) {
+                    window.alert("Select a valid redeem amount")
+                  } else {
+                    setShowSpin(true)
+                    const result = await props.tx(props.writeContracts.LSP.redeem(utils.parseUnits(redeemVal.toString(), 6)))
+                    .then((e) => setShowSpin(false))
+                  }
+                }}
+              danger>
+                { showSpin ? <Spin /> : "Redeem Pairs" }
+              </Button>
+              <Button disabled >  { showSpin ? <Spin /> : "Settle Longs/Shorts" }</Button>
+            </Card>
           </Col>
         </Row>
     </div>
